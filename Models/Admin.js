366f478af57af4efd390292056b482b61bc14244
Kristijan Vasic <157734449+vasickiko@users.noreply.github.com>
2026-04-11 11:24:40 +0200

    Creating Admins

--- Models/Admin.js
+++ Models/Admin.js
@@ -12,28 +12,65 @@ const findAdmin = async (username) => {
   return await Admin.findOne({ username });
 };
 
-const createAdmin = async () => {
-  try {
-    const existingAdmin = await Admin.findOne({ username: "admin" });
+// const createAdmin = async () => {
+//   try {
+//     const existingAdmin = await Admin.findOne({ username: "admin" });
 
-    if (existingAdmin) {
-      console.log("Admin already exists");
+//     if (existingAdmin) {
+//       console.log("Admin already exists");
+//       return;
+//     }
+
+//     const hashedPassword = await bcrypt.hash("admin123", 10);
+
+//     const admin = new Admin({
+//       username: "admin",
+//       password: hashedPassword,
+//     });
+
+//     await admin.save();
+    
+//     console.log("Admin created");
+//   } catch (err) {
+//     console.log("Error creating admin:", err);
+//   }
+// };
+
+
+const createTwoAdmins = async () => {
+  try { 
+
+    const existingAdmin1 = await Admin.findOne({ username: "admin" });
+    const existingAdmin2 = await Admin.findOne({ username: "admin2" });
+
+    if (existingAdmin1 || existingAdmin2) {
+      console.log("Admins ready to log in");
       return;
     }
 
-    const hashedPassword = await bcrypt.hash("admin123", 10);
+    const adminsData = [
+      { username: "admin", password: "sdKASJJidj2Sj29SJkjs" },
+      { username: "admin2", password: "SKHjd828as892Sjif2h" },
+    ];
 
-    const admin = new Admin({
-      username: "admin",
-      password: hashedPassword,
+    for (const adminData of adminsData) {
+      const hashedPassword = await bcrypt.hash(adminData.password, 10);
+
+      const admin = new Admin({
+        username: adminData.username,
+        password: hashedPassword,
     });
 
     await admin.save();
     
     console.log("Admin created");
+    }
   } catch (err) {
     console.log("Error creating admin:", err);
   }
 };
+
+
+
 
 module.exports = { createAdmin, findAdmin };
